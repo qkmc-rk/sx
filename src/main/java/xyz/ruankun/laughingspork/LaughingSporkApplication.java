8cf4bddd39dbef248bee17bc252840256cef8c6c
mayberk <qkmc@outlook.com>
2019-10-16 10:30:08 +0800

    first commit

---
+++ src/main/java/xyz/ruankun/laughingspork/LaughingSporkApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruankun.laughingspork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaughingSporkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaughingSporkApplication.class, args);
+	}
+
+}
