afba9611dde7f1d6b0859d2c689f85c08e461e13
innovation <578777205@qq.com>
2019-10-23 13:18:28 +0800

    Add files via upload

--- src/main/java/xyz/ruankun/laughingspork/repository/SxCollegePrincipalRepository.java
+++ src/main/java/xyz/ruankun/laughingspork/repository/SxCollegePrincipalRepository.java
@@ -1,8 +1,8 @@
 package xyz.ruankun.laughingspork.repository;
 
 import xyz.ruankun.laughingspork.entity.SxCollegePrincipal;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+        import org.springframework.stereotype.Repository;
+        import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface SxCollegePrincipalRepository extends JpaRepository<SxCollegePrincipal, Long>{
